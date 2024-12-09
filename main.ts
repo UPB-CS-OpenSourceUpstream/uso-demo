180396a45b6dc001d4d17aa819e9fa028601a273
Alexandru RADOVICI <alexandru.radovici@wyliodrin.com>
2024-12-09 17:19:23 +0200

    division

--- main.ts
+++ main.ts
@@ -7,3 +7,13 @@ export function add(a: number, b: number): number {
 export function sub(a: number, b: number): number {
   return a - b;
 }
+
+// function divides two numbers
+export function div(a: number, b: number): number {
+  return a / b;
+}
+
+// function divides two integer numbers
+export function intDiv(a: number, b: number): number {
+  return Math.floor(a / b);
+}
