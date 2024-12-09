973d067b3390b9c3437ebeb82b41a704d25a3968
Alexandru RADOVICI <alexandru.radovici@wyliodrin.com>
2024-12-09 17:20:20 +0200

    format

--- main_test.ts
+++ main_test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "@std/assert";
-import { add, sub, div, intDiv } from "./main.ts";
+import { add, div, intDiv, sub } from "./main.ts";
 
 Deno.test(function addTest() {
   assertEquals(add(2, 3), 5);
