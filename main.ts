3dfc8721919be82b008f950a9f00a307989eb03b
Alexandru RADOVICI <alexandru.radovici@wyliodrin.com>
2024-12-09 16:45:56 +0200

    add doc

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
+// function adds two numbers
 export function add(a: number, b: number): number {
   return a + b;
 }
 
+// function subtracts two numbers
 export function sub(a: number, b: number): number {
   return a - b;
 }
